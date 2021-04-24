64d0ddce2ad9baab7f12a8b5498f736261406fd0
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-04-24 00:26:10 -0300

    criando funcionalidade de criar uma nova tarefa

--- front/src/main.js
+++ front/src/main.js
@@ -1,25 +1,35 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import Todos from './api/todos';
-import './assets/css/main.css'
+import './assets/css/main.css';
 
 const apiTodos = new Todos();
 
 const app = createApp({
   data: () => ({
-    todos: []
+    todos: [],
+    form: {
+      text: '',
+      done: false
+    }
   }),
 
   created() {
-    this.fetchTodos()
+    this.fetchTodos();
   },
 
   methods: {
     async fetchTodos() {
-      const { data } = await apiTodos.list()
-      this.todos = data
-      console.log(this.todos)
-    }
-  }
-})
+      const { data } = await apiTodos.list();
+      this.todos = data;
+      console.log(this.todos);
+    },
 
-app.mount('#app')
+    async createdTodo() {
+      const { data } = await apiTodos.create(this.form);
+      this.todos.push(data)
+      this.form = ""
+    },
+  },
+});
+
+app.mount('#app');
